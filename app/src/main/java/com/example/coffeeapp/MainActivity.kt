068936a0dea124353c5eb28e05d7105c933d7cef
burak <brkdewils@gmail.com>
2021-12-25 22:31:57 +0300

    a lot of properties added!

--- app/src/main/java/com/example/coffeeapp/MainActivity.kt
+++ app/src/main/java/com/example/coffeeapp/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.coffeeapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        button.setOnClickListener {
+            val inten = Intent(this,ProfileActivity::class.java)
+            startActivity(inten)
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/coffeeapp/ProfileActivity.kt
@@ -0,0 +1,20 @@
+package com.example.coffeeapp
+
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_profile.*
+
+class ProfileActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+        floatingActionButton1.imageTintList = ColorStateList.valueOf(Color.rgb(255,50,50))
+
+    }
+}
